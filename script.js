eec04144f3aad69b2f053ba64e1b70bf0ba03051
Papaya Delight <jasmineteters2020@gmail.com>
2020-10-03 19:17:21 -0600

    final formatting

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function generatePassword() {
     var askSpecialChar = confirm("Do you want to include symbols?");
     var askAlphaUpper = confirm("Do you want to include alphaUpper letters?");
     var askNumberStr = confirm("Do you want to include numbers?");
-
     var numberStr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -35,16 +34,13 @@ function generatePassword() {
   }
 }
 
-
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
